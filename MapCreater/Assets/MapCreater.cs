3e8aaba03f7c02b917589a6041df2189e1957165
KID <50666207+KID421@users.noreply.github.com>
2019-07-03 11:51:48 +0800

    完成地圖編輯器、製作關卡 1 與關卡 2

--- MapCreater/Assets/MapCreater.cs
+++ MapCreater/Assets/MapCreater.cs
@@ -6,6 +6,11 @@ public class MapCreater : MonoBehaviour
 
     public GameObject Ground, Coin, Prop;
 
+    public Color Brown, Yellow, Green;
+
+    /// <summary>
+    /// 實例化地圖
+    /// </summary>
     private void InstantiateMap()
     {
         int width = Map.width;
@@ -23,8 +28,44 @@ public class MapCreater : MonoBehaviour
                 }
 
                 Debug.Log("圖片的顏色：" + Map.GetPixel(x, y));
+
+                if (Map.GetPixel(x, y) == Brown)
+                {
+                    //GameObject temp = Instantiate(Ground, new Vector2(x, y) * 2, Quaternion.identity);
+                    //temp.AddComponent<SceneObject>();
+                    //temp.GetComponent<SceneObject>().objectType = ObjectType.Ground;
+                    CreateObject(Ground, ObjectType.Ground, x, y);
+                }
+                else if (Map.GetPixel(x, y) == Yellow)
+                {
+                    //GameObject temp = Instantiate(Coin, new Vector2(x, y) * 2, Quaternion.identity);
+                    //temp.AddComponent<SceneObject>();
+                    //temp.GetComponent<SceneObject>().objectType = ObjectType.Coin;
+                    CreateObject(Coin, ObjectType.Coin, x, y);
+                }
+                else if (Map.GetPixel(x, y) == Green)
+                {
+                    //GameObject temp = Instantiate(Prop, new Vector2(x, y) * 2, Quaternion.identity);
+                    //temp.AddComponent<SceneObject>();
+                    //temp.GetComponent<SceneObject>().objectType = ObjectType.Prop;
+                    CreateObject(Prop, ObjectType.Prop, x, y);
+                }
             }
         }
+    }
+
+    /// <summary>
+    /// 建立物件 - 指定物件、物件類型、座標
+    /// </summary>
+    /// <param name="obj">指定物件</param>
+    /// <param name="type">物件類型</param>
+    /// <param name="x">左右</param>
+    /// <param name="y">上下</param>
+    private void CreateObject(GameObject obj, ObjectType type, int x, int y)
+    {
+        GameObject temp = Instantiate(obj, new Vector2(x, y) * 2, Quaternion.identity);
+        temp.AddComponent<SceneObject>();
+        temp.GetComponent<SceneObject>().objectType = type;
     }
 
     private void Start()
---
+++ MapCreater/Assets/SceneObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public enum ObjectType { Ground, Coin, Prop }
+
+public class SceneObject : MonoBehaviour
+{
+    public ObjectType objectType;
+}
