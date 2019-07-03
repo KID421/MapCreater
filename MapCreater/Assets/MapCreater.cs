f6ac21eb35113b90f1ed27f9a26436a9fd9d9f00
KID <50666207+KID421@users.noreply.github.com>
2019-07-03 09:46:01 +0800

    地圖編輯器：讀取圖片顏色資訊
    建立專案、匯入素材、地圖編輯器欄位與讀取圖片顏色資訊

---
+++ MapCreater/Assets/MapCreater.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MapCreater : MonoBehaviour
+{
+    public Texture2D Map;
+
+    public GameObject Ground, Coin, Prop;
+
+    private void InstantiateMap()
+    {
+        int width = Map.width;
+        int height = Map.height;
+        Debug.Log("圖片的寬：" + width);
+        Debug.Log("圖片的高：" + height);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (Map.GetPixel(x, y).a == 0)
+                {
+                    continue;
+                }
+
+                Debug.Log("圖片的顏色：" + Map.GetPixel(x, y));
+            }
+        }
+    }
+
+    private void Start()
+    {
+        InstantiateMap();
+    }
+}
